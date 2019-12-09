bf8307ced4a270493a753b212b20b64a8ca7f7a6
shadab4150 <37144992+shadab4150@users.noreply.github.com>
2019-12-09 23:55:46 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1-SpC5SovwztKQ82xD0BrPyUeXroYasjD'
-export_file_name = 'export.pkl'
+export_file_url = "https://drive.google.com/uc?export=download&id=1-SpC5SovwztKQ82xD0BrPyUeXroYasjD"
+export_file_name = "export.pkl"
 
-classes = ['adidas','nike','puma']
+classes = ['Adidas sneakers','Nike sneakers','Puma sneakers']
 path = Path(__file__).parent
 
 app = Starlette()
